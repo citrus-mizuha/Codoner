3cbffe650c9d3bea3dd570339a16ef20129d78a6
kimuti-tsukai <168959212+kimuti-tsukai@users.noreply.github.com>
2024-07-13 14:14:47 +0900

    Change codon_table global

--- Gene_codon/Fixed-refer-memo/Rust_fixed_encoder/src/main.rs
+++ Gene_codon/Fixed-refer-memo/Rust_fixed_encoder/src/main.rs
@@ -2,6 +2,34 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::collections::HashMap;
 use std::io::{self, Write};
+use std::sync::OnceLock;
+
+// Global codon_table
+static CODON_TABLE: OnceLock<HashMap<u8, Vec<&str>>> = OnceLock::new();
+
+// Init codon_table
+fn init_codon_table() {
+    let codon_table: HashMap<u8, Vec<&str>> = [
+        (0, vec!["000", "001", "002", "003"]),
+        (1, vec!["010", "011", "012", "013"]),
+        (2, vec!["020", "021", "022", "023"]),
+        (3, vec!["030", "031", "032", "033"]),
+        (4, vec!["100", "101", "102", "103"]),
+        (5, vec!["110", "111", "112", "113"]),
+        (6, vec!["120", "121", "122", "123"]),
+        (7, vec!["130", "131", "132", "133"]),
+        (8, vec!["200", "201", "202", "203"]),
+        (9, vec!["210", "211", "212", "213"]),
+        (10, vec!["220", "221", "222", "223"]),
+        (11, vec!["230", "231", "232"]),
+        (12, vec!["233", "300", "301"]),
+        (13, vec!["302", "303", "310"]),
+        (14, vec!["311", "312", "313"]),
+        (15, vec!["320", "321", "322"]),
+    ].iter().cloned().collect();
+
+    CODON_TABLE.set(codon_table).expect("Somehing went wrong. Conflict may happend.");
+}
 
 fn string_to_binary(input_string: &str) -> String {
     input_string
@@ -19,11 +47,11 @@ fn split_into_chunks(binary_string: &str) -> Vec<String> {
         .collect()
 }
 
-fn encrypt_data(chunks: Vec<String>, codon_table: &HashMap<u8, Vec<&str>>) -> Vec<String> {
+fn encrypt_data(chunks: Vec<String>) -> Vec<String> {
     let mut encrypted_data: Vec<String> = Vec::new();
     for chunk in chunks {
         if let Ok(value) = u8::from_str_radix(&chunk, 2) {
-            if let Some(codon_choices) = codon_table.get(&value) {
+            if let Some(codon_choices) = CODON_TABLE.get().unwrap().get(&value) {
                 let chosen_codon = codon_choices.choose(&mut thread_rng()).unwrap_or(&"Invalid").to_string();
                 encrypted_data.push(chosen_codon);
             } else {
@@ -46,24 +74,7 @@ fn convert_to_binary(encrypted_data: Vec<String>) -> String {
 }
 
 fn main() {
-    let codon_table: HashMap<u8, Vec<&str>> = [
-        (0, vec!["000", "001", "002", "003"]),
-        (1, vec!["010", "011", "012", "013"]),
-        (2, vec!["020", "021", "022", "023"]),
-        (3, vec!["030", "031", "032", "033"]),
-        (4, vec!["100", "101", "102", "103"]),
-        (5, vec!["110", "111", "112", "113"]),
-        (6, vec!["120", "121", "122", "123"]),
-        (7, vec!["130", "131", "132", "133"]),
-        (8, vec!["200", "201", "202", "203"]),
-        (9, vec!["210", "211", "212", "213"]),
-        (10, vec!["220", "221", "222", "223"]),
-        (11, vec!["230", "231", "232"]),
-        (12, vec!["233", "300", "301"]),
-        (13, vec!["302", "303", "310"]),
-        (14, vec!["311", "312", "313"]),
-        (15, vec!["320", "321", "322"]),
-    ].iter().cloned().collect();
+    init_codon_table();
 
     let mut input_string = String::new();
     print!("文字列 : ");
@@ -77,7 +88,7 @@ fn main() {
     let binary_chunks = split_into_chunks(&utf8_binary);
     println!("Binary Chunks: {:?}", binary_chunks);
 
-    let encrypted_data = encrypt_data(binary_chunks, &codon_table);
+    let encrypted_data = encrypt_data(binary_chunks);
     println!("Pseudo-Quaternary: {:?}", encrypted_data);
 
     let converted_binary = convert_to_binary(encrypted_data);
